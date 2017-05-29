4ee59416a40d0c2d74fd0ccefbd7acb3f36ff0de
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-05-29 21:59:24 +0900

    Groupモデルの作成、DB名変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527075007) do
+ActiveRecord::Schema.define(version: 20170529124945) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "key",        limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -27,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170527075007) do
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.string   "name",                   limit: 255
+    t.integer  "group_id",               limit: 4
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

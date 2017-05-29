b3d1d7c288ea596e2f86d85b2b16f965d9f1cc0f
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-05-29 22:32:57 +0900

    ユーザーとグループの紐付け、およびグループ名による認証機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys:[:group_key])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :group_key])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ActiveRecord::Base
+
+  #association
+  has_many :users
 end

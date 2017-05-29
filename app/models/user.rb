932bfe2ba700cce08048b11f23548da6a24dbeed
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-05-29 21:47:02 +0900

    認証のフォームにグループ名の欄を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+
+  #accessor
+  attr_accessor :group_key
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,

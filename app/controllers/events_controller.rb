8ea96947461092f9e02a03a1ab2cf34444239df1
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-05-31 19:43:05 +0900

    eventのCRUD機能を実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :start, :end, :allDay)
+      params.require(:event).permit(:title, :start, :end, :allDay).merge(color: "#94DBFC", user_id: current_user.id, group_id: current_user.group_id, common: false)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,24 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+    @events = Event.all.map{|e|
+    {
+    :id => e.id,
+    :title => e.title,
+    :start => e.start,
+    :end => e.end,
+    :color => e.color,
+    :allDay => e.allDay,
+    :url => event_path(e.id)
+    }
+    }
+
+    respond_to do |format|
+    format.html
+    format.xml { render :xml => @events }
+    format.json { render :json => @events }
+    end
   end
 
   def top
@@ -10,6 +28,23 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where(group_id: current_user.group_id)
+    @events = Event.all.map{|e|
+    {
+    :id => e.id,
+    :title => e.title,
+    :start => e.start,
+    :end => e.end,
+    :color => e.color,
+    :allDay => e.allDay,
+    :url => event_path(e.id)
+    }
+    }
+
+    respond_to do |format|
+    format.html
+    format.xml { render :xml => @events }
+    format.json { render :json => @events }
+    end
   end
 
 end

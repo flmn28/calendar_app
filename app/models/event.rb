1d1ded68bdb6fc4d63159fa041056ec6f3a15096
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-06-01 14:03:36 +0900

    Eventのバリデーションを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
 
+  validate :add_error_message
+
+  def add_error_message
+    if title.empty?
+      errors[:base] << "タイトルを入力してください"
+    end
+  end
+
 end
